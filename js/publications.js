b6bd21a073750c71461990951f9c69d6581ec953
Aditya Prakash <prakashaditya369@gmail.com>
2022-11-19 00:20:57 +0530

    Add conference papers IAC

--- js/publications.js
+++ js/publications.js
@@ -42,7 +42,30 @@ const publication_data = {
             url: ""
         }
     }],
-    Conference: []
+    Conference: [{
+        title: "Control Design for transfer of payload between reusable rocket and lower end of the Skyhook",
+        authors: ["A. Prakash", "D. K. Giri"],
+        journal: "73rd International Aeronautical Congress (IAC)",
+        volume: "",
+        number: "",
+        pages: "",
+        year: 2022,
+        DOI: "https://iafastro.directory/iac/paper/id/70859/ext/abstract-pdf/IAC-22,D1,3,4,x70859.brief.pdf",
+        related_blog: {title: "",
+                        url: ""}
+    },
+    {
+        title: "MagLev based 3-DOF experimental Platform for Autonomous Spacecraft Rendezvous and Docking",
+        authors: ["N. Jaggi", "A. Prakash", "G. Kumar", "P. Dubey", "D. K. Giri"],
+        journal: "73rd International Aeronautical Congress (IAC)",
+        volume: "",
+        number: "",
+        pages: "",
+        year: 2022,
+        DOI: "https://iafastro.directory/iac/paper/id/70859/ext/abstract-pdf/IAC-22,D1,3,4,x70859.brief.pdf",
+        related_blog: {title: "",
+                        url: ""}
+    }]
 }
 
 function create_element(data_, main_) {
