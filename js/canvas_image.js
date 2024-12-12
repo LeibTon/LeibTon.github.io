17b9fcc1fcc46d2d2387e7e44e3ea88d968bd32c
Aditya Prakash <leibton@gatech.edu>
2024-12-12 11:40:16 -0500

    fixed the image issue in about me page

--- js/canvas_image.js
+++ js/canvas_image.js
@@ -6,8 +6,9 @@ var canvas_control = false;
 // Resizing
 canvas.height = window.innerHeight;
 canvas.width = canvas_main_box.clientWidth;
-var velocity = 0.5;
+var velocity = 0.4;
 var resize = 1.9;
+let chkTick = 10;
 let particleArray = [];
 let mouse = {
     x: undefined,
@@ -36,22 +37,22 @@ canvas.addEventListener("mouseup", () => {
 })
 
 /*Main function to control particles representing pixels*/
-function Draw() 
-{
+function Draw() {
     let imageWidth = png.width;
     let imageHeight = png.height;
     const data = ctx.getImageData(0, 0, imageWidth, imageHeight);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     function Particle(x, y, dy, colour) {
         this.x = x + canvas.width - resize * png.width - 50;
-        this.y = Math.random() * (canvas.height) + y + canvas.height/2 - resize * png.height/2;
+        this.y = Math.random() * (canvas.height) + y + canvas.height / 2 - resize * png.height / 2;
         this.basex = x + canvas.width - resize * png.width - 50;
-        this.basey = y + canvas.height/2 - resize * png.height/2;
+        this.basey = y + canvas.height / 2 - resize * png.height / 2;
         this.dv = dy
         this.dy = dy
         this.dx = dy
         this.velocity = 30
-        this.size = 1.1;
+        this.size = 1.6;
         this.colour = colour;
         this.draw = function () {
             ctx.beginPath();
@@ -65,21 +66,36 @@ function Draw()
                 this.y = this.basey + Math.random() * (mouse.y - mouse.fixedPosy + 1);
                 this.velocity = 12;
             } else {
-                this.velocity -= this.dv
-
-                if (this.y !== this.basey) {
-                    this.dy = (this.basey - this.y) / this.velocity
-                    this.y += this.dy;
-                }
-                
-    
-                if (Math.abs(this.x - this.basex) > 5) {
-                    this.dx = (this.basex - this.x) / this.velocity
+                this.velocity -= this.dv;
+                // this.velocity = Math.max(this.velocity, 1);
+                if (Math.abs(this.x - this.basex) > 2.5 || Math.abs(this.y - this.basey) > 2.5) {
+                    // Move particle towards its base position
+                    this.dx = (this.basex - this.x) / this.velocity;
+                    this.dy = (this.basey - this.y) / this.velocity;
                     this.x += this.dx;
+                    this.y += this.dy;
+                } else {
+                    if (chkTick % 10 === 0) {
+                        this.x = this.basex + (Math.random() - 0.5) * 3;
+                        this.y = this.basey + (Math.random() - 0.5) * 3;
+                        chkTick = 0;
+                    }
+                    chkTick++;
                 }
-                else{
-                    this.x = this.basex + 5 * Math.random()
-                }
+
+                // if (this.y !== this.basey) {
+                //     this.dy = (this.basey - this.y) / this.velocity
+                //     this.y += this.dy;
+                // }
+
+
+                // if (Math.abs(this.x - this.basex) > 5) {
+                //     this.dx = (this.basex - this.x) / this.velocity
+                //     this.x += this.dx;
+                // }
+                // else{
+                //     this.x = this.basex + 5 * Math.random()
+                // }
 
             }
 
@@ -100,12 +116,15 @@ function Draw()
                     let g = data.data[(y * 4 * data.width) + (x * 4) + 1];
                     let b = data.data[(y * 4 * data.width) + (x * 4) + 2];
                     let avg = (r + g + b) / 3;
-                    let color = "rgba(" + (avg / 251 * 255) + "," + (avg / 251 * 245) + "," + (avg / 251 * 253) + "," + data.data[(y * 4 * data.width) + (x * 4) + 3] + ")";
+                    let color = "rgba(" + (avg / 251 * 255) + "," + (avg / 251 * 245) + "," + (avg / 251 * 253) + "," +
+                        data.data[(y * 4 * data.width) + (x * 4) + 3] + ")";
                     particleArray.push(new Particle(positionX * resize, positionY * resize, velocity, color));
                 } else if (data.data[(y * 4 * data.width) + (x * 4) + 3] > 50) {
                     let positionX = x;
                     let positionY = y;
-                    let color = "rgba(" + (2 * 255 + data.data[(y * 4 * data.width) + (x * 4)]) / 3 + "," + (2 * 245 + data.data[(y * 4 * data.width) + (x * 4) + 1]) / 3 + "," + (2 * 253 + data.data[(y * 4 * data.width) + (x * 4) + 2]) / 3 + ",10)";
+                    let color = "rgba(" + (2 * 255 + data.data[(y * 4 * data.width) + (x * 4)]) / 3 + "," + (2 * 245 +
+                        data.data[(y * 4 * data.width) + (x * 4) + 1]) / 3 + "," + (2 * 253 + data.data[(y * 4 *
+                        data.width) + (x * 4) + 2]) / 3 + ",10)";
                     particleArray.push(new Particle(positionX * resize, positionY * resize, velocity, color));
                 }
             }
@@ -116,7 +135,7 @@ function Draw()
         if (canvas_control) {
             requestAnimationFrame(animate);
             ctx.clearRect(0, 0, innerWidth, innerHeight)
-            for (let i = 0; i < particleArray.length; i+=2) {
+            for (let i = 0; i < particleArray.length; i += 2) {
                 particleArray[i].update();
             }
         }
